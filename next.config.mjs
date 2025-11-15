3ecf56b5698ac52d1f13b53796bcef8307c09935
Kartik Maandothiya <kartikya27@gmail.com>
2025-11-15 21:21:54 +0530

    add yaml

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,8 @@ const nextConfig = {
   experimental: {
     allowedDevOrigins: [
       'http://localhost:3000',
-      'http://13.48.84.179:3001',
+      'https://13.48.84.179:3001',
       'https://dev.testyourgerman.com',
-      'https://testyourgerman-flpexam-api-env.eba-erbuwieg.eu-north-1.elasticbeanstalk.com'
     ],
   },
 };
