2f4ea7b2169cea502a0397affea57c88af98b90f
Kartik Maandothiya <kartikya27@gmail.com>
2025-11-15 20:49:54 +0530

    add yaml

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    allowedDevOrigins: [
+      'http://localhost:3000',
+      'http://13.48.84.179:3001',
+      'https://dev.testyourgerman.com',
+    ],
+  },
+};
 
 export default nextConfig;
